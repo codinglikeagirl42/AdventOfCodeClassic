27293512f202ac08631152406852071ec612692d
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-14 23:35:39 -0500

    Added solution for part 2

--- AOC2020/Day1/day1.py
+++ AOC2020/Day1/day1.py
@@ -3,5 +3,6 @@ data = open("AOC2020/Day1/day1.txt", "r").read().split("\n")
 
 for line in data:
     for lines in data:
-        if int(line) + int(lines) == 2020:
-            print((int(line)) * (int(lines)))
+        for more_lines in data:
+            if int(line) + int(lines) + int(more_lines) == 2020:
+                print((int(line)) * (int(lines)) * (int(more_lines)))
