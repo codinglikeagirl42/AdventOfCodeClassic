b257b84b7155b87e5b1934b20028274852448ad3
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-14 23:39:53 -0500

    Formatting output

--- AOC2020/Day1/day1.py
+++ AOC2020/Day1/day1.py
@@ -1,8 +1,10 @@
 #Get data from text file
 data = open("AOC2020/Day1/day1.txt", "r").read().split("\n")
-
+#Code does not account for multiplies
 for line in data:
     for lines in data:
+        if int(line) + int(lines) == 2020:
+            print("Part 1: " + str((int(line)) * (int(lines))))
         for more_lines in data:
             if int(line) + int(lines) + int(more_lines) == 2020:
-                print((int(line)) * (int(lines)) * (int(more_lines)))
+                print("Part 2: " + str((int(line)) * (int(lines)) * (int(more_lines))))
