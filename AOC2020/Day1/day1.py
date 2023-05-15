57f5d8c09288879ef602215fe244b108c2dc916a
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-14 22:19:21 -0500

    read in file

---
+++ AOC2020/Day1/day1.py
@@ -0,0 +1,13 @@
+#Get data from text file
+data = open("AOC2020/Day1/test.txt", "r").read().split("\n")
+
+from itertools import combinations
+
+
+print(type(data))
+
+#all_pairs = combinations(data, 2)
+
+    
+  
+#print(all_pairs)
