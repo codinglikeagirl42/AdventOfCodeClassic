e05f0f350cc75be41c85b4ce533119ab7b8e88c9
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-14 23:44:24 -0500

    Fixed issue with multiples

--- AOC2020/Day1/day1.py
+++ AOC2020/Day1/day1.py
@@ -1,10 +1,15 @@
 #Get data from text file
 data = open("AOC2020/Day1/day1.txt", "r").read().split("\n")
 #Code does not account for multiplies
+part1 = 0
+part2 = 0
 for line in data:
     for lines in data:
         if int(line) + int(lines) == 2020:
-            print("Part 1: " + str((int(line)) * (int(lines))))
+            part1 =  (int(line)) * (int(lines))
         for more_lines in data:
             if int(line) + int(lines) + int(more_lines) == 2020:
-                print("Part 2: " + str((int(line)) * (int(lines)) * (int(more_lines))))
+                part2 = (int(line)) * (int(lines)) * (int(more_lines))
+
+print("Part 1: " + str(part1))
+print("Part 2: " + str(part2))
