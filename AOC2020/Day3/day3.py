0fcaf8e6e807992f6622b7a6b3cdaa0166431af5
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-16 23:54:20 -0500

    Started part2

--- AOC2020/Day3/day3.py
+++ AOC2020/Day3/day3.py
@@ -10,29 +10,38 @@ print(columns)
 
 # Number of Rows (y)
 rows = (len(data[0]))
-print(rows)
-tree = "#"
-trees = 0
-x = 0
-y = 0
-x_move = 3
-y_move = 1
+#print(rows)
 
-for row in range(columns-1):
-    x += x_move
-    if rows <= x:
-        x = x-rows
-    y += y_move
+#x_move = 3
+#y_move = 1
 
-    #print(x)
-    #print(y)
-    print(data[y][x])
+def total_trees(x_move, y_move):
+
+    tree = "#"
+    trees = 0
+    x = 0
+    y = 0
+
+    for row in range(columns-1):
+        x += x_move
+        if rows <= x:
+            x = x-rows
+        y += y_move
+
+        #print(x)
+        #print(y)
+        #print(data[y][x])
+        
+
+        if data[y][x] == tree:
+            trees += 1
+    return trees
+
     
+trees_part1 = total_trees(3,1)  
+print("Part 1: " + str(trees_part1))
 
-    if data[y][x] == tree:
-        trees += 1
-
-
-    
-  
-print("Part 1: " + str(trees))
+trees_part2 = total_trees(3,1) * total_trees(1,1) * total_trees(5,1) * total_trees(7,1) 
+#trees_more = total_trees(1,2) 
+print("Part 2: " + str(trees_part2))
+#print(trees_more)
