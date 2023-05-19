fa631c61b8b8df56f8fad94360b95cdfebd264c2
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-18 21:21:22 -0500

    part2 working but still needs remix

--- AOC2020/Day4/day4re.py
+++ AOC2020/Day4/day4re.py
@@ -4,7 +4,7 @@ import re
 data = open("AOC2020/Day4/day4.txt", "r").read().split("\n\n")
 
 passports_valid = []
-new_passport_valid = []
+new_passport_valid = 0
 
 for line in data:
     new_lines = line.split( )
@@ -17,29 +17,36 @@ for line in data:
     if len(new_lines) == 7:
         passports_valid.append(new_lines)
 #part 2       
-pieces = []
+
 eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 for passport in passports_valid:
     sorted_passport = sorted(passport)
-    if len(sorted_passport) == 8:
-        del sorted_passport[1]
+    eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
     pieces = []
     new_count = 0
     eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
     for items in sorted_passport:
         pieces.append(items[4:])  
     if 1920 <= int(pieces[0]) <= 2002:
-        if pieces[1] in eye_colors:
-            if 2020 <= int(pieces[2]) <= 2030:
-                if len(pieces[3]) == 7:
-                    #https://docs.python.org/3/howto/regex.html#regex-howto
-                    if re.match("^#[0-9a-f${6}]", pieces[3][1:]):   
-                        if pieces[4][-2:] == "cm" and 150 <= int(pieces[4][:-2])<=193 or pieces[4][-2:] == "in" and 59 <= int(pieces[4][:-2])<=76:
-                            if 2010 <= int(pieces[5]) <= 2020:
-                                if re.match("^[0-9${9}]", pieces[6]) and len(pieces[6]) == 9:
-                                    new_passport_valid.append(passport)
-    
-     
+        new_count += 1
+    if pieces[1] in eye_colors:
+        new_count += 1
+    if 2020 <= int(pieces[2]) <= 2030:
+        new_count += 1
+    if len(pieces[3]) == 7 and pieces[3][0] == "#":
+        #https://docs.python.org/3/howto/regex.html#regex-howto
+        if re.match("^[0-9a-f${6}]", pieces[3][1:]):
+            new_count += 1
+    if pieces[4][-2:] == "cm" and 150 <= int(pieces[4][:-2])<=193:
+        new_count += 1
+    if pieces[4][-2:] == "in" and 59 <= int(pieces[4][:-2])<=76:
+        new_count += 1
+    if 2010 <= int(pieces[5]) <= 2020:
+        new_count += 1
+    if re.match("^[0-9${9}]", pieces[6]) and len(pieces[6]) == 9:
+        new_count += 1
+    if new_count == 7:
+        new_passport_valid += 1 
 print("Part 1: " + str(len(passports_valid)))
-print("Part 2: " + str(len(new_passport_valid)))
+print("Part 2: " + str(new_passport_valid))
  
