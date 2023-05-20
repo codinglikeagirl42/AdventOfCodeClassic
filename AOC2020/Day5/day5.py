6cdc5548040622027b0d63ffe80c175947b58c0d
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-19 23:41:11 -0500

    started day5

--- AOC2020/Day5/day5.py
+++ AOC2020/Day5/day5.py
@@ -1,2 +1,7 @@
-#Get data from text file, blank line is just two new lines.
-data = open("AOC2020/Day5/test.txt", "r").read().split("\n\n")
+#Get data from text file
+data = open("AOC2020/Day5/test.txt", "r").read().split("\n")
+
+
+for boarding_pass in data:
+    for i in boarding_pass:
+
