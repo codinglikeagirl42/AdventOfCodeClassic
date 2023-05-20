ae7442b24092f581fb9032e59332926874dbd3c7
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-19 23:59:09 -0500

    First attempt day 1

--- AOC2019/day1.py
+++ AOC2019/day1.py
@@ -1,2 +1,13 @@
 #Get data from text file
-data = open("AOC2019/Day1/day1.txt", "r").read().split("\n")
+data = open("AOC2019/day1.txt", "r").read().split("\n")
+
+from math import floor
+#https://favtutor.com/blogs/round-down-python#:~:text=Python%27s%20floor%20division%20operator%2C%20aka,with%20the%20floor()%20method.
+
+fuel_need = 0
+for number in data:
+    
+    fuel_need += (floor((int(number))/3))
+    #print(type(floor(int(number/3)) - 2))
+
+print("Part1: " + str(fuel_need))
