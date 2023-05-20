7bb58dde15ca912cb105ebddf5a782c24ee2ee21
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-20 00:50:22 -0500

    Moved day1 into folder and setup day2

--- AOC2019/Day1/day1.py
+++ AOC2019/Day1/day1.py
@@ -1,5 +1,5 @@
 #Get data from text file
-data = open("AOC2019/day1.txt", "r").read().split("\n")
+data = open("AOC2019/Day1/day1.txt", "r").read().split("\n")
 
 from math import floor
 #https://favtutor.com/blogs/round-down-python#:~:text=Python%27s%20floor%20division%20operator%2C%20aka,with%20the%20floor()%20method.
---
+++ AOC2019/Day2/day2.py
@@ -0,0 +1,2 @@
+#Get data from text file
+data = open("AOC2020/Day2/test.txt", "r").read().split("\n")
