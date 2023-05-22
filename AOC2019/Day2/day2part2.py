e1abd0885ec07e24a70cd638bae46423548e698d
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-21 22:12:22 -0500

    First attempt part2

---
+++ AOC2019/Day2/day2part2.py
@@ -0,0 +1,34 @@
+#Get data from text file
+data = open("AOC2019/Day2/day2.txt", "r").read().split(",")
+
+def pairs_needed(data):
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+
+            data[1] = noun
+            data[2] = verb
+            answer = intcode(data)
+            if answer[0] == 19690720:
+                return(100 * noun + verb)
+
+
+
+def intcode(data):
+    for opcode in range(0, len(data), 4):
+        opcode = int(opcode)
+        what_to_do = int(data[opcode])
+        input_1 = int(data[opcode + 1])
+        input_2 = int(data[opcode + 2])
+        where_to_put = int(data[opcode + 3])
+
+        if what_to_do == 1:
+            data[where_to_put] = int(data[input_1]) + int(data[input_2])
+        elif what_to_do == 2:
+            data[where_to_put] = int(data[input_1]) * int(data[input_2])
+        else:
+            print(what_to_do)
+            return(data)
+
+
+answer_2 = pairs_needed(data)
+print("Part 2: " + str(answer_2))
