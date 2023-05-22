58c65b549d46c36834bc5fa7b950979e9f424b67
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-21 22:59:40 -0500

    Still trying to debug part2

--- AOC2019/Day2/day2part2.py
+++ AOC2019/Day2/day2part2.py
@@ -1,34 +1,50 @@
 #Get data from text file
 data = open("AOC2019/Day2/day2.txt", "r").read().split(",")
-
+"""
 def pairs_needed(data):
-    for noun in range(0, 100):
-        for verb in range(0, 100):
+    for noun in range(100):
+        for verb in range(100):
+            
+            print(type(noun))
+            print(verb)
+            print("**************")
+           
+            data[1] = str(noun)
+            data[2] = str(verb)
+            #print(data[0:3])
+            #print("************************")
+           
+            
+            answer = intcode(data)
+            print(answer)
+            #if answer == 19690720:
+                #return(100 * noun + verb)
+           
+"""
+
+def intcode(data):
+
+    for noun in range(100):
+        for verb in range(100):
 
             data[1] = noun
             data[2] = verb
-            answer = intcode(data)
-            if answer[0] == 19690720:
+    
+            for opcode in range(0, len(data), 4):
+                opcode = int(opcode)
+                what_to_do = int(data[opcode])
+                input_1 = int(data[opcode + 1])
+                input_2 = int(data[opcode + 2])
+                where_to_put = int(data[opcode + 3])
+
+                if what_to_do == 1:
+                    data[where_to_put] = int(data[input_1]) + int(data[input_2])
+                elif what_to_do == 2:
+                    data[where_to_put] = int(data[input_1]) * int(data[input_2])
+                else:
+                    break
+            if data[0] == 19690720:
                 return(100 * noun + verb)
 
-
-
-def intcode(data):
-    for opcode in range(0, len(data), 4):
-        opcode = int(opcode)
-        what_to_do = int(data[opcode])
-        input_1 = int(data[opcode + 1])
-        input_2 = int(data[opcode + 2])
-        where_to_put = int(data[opcode + 3])
-
-        if what_to_do == 1:
-            data[where_to_put] = int(data[input_1]) + int(data[input_2])
-        elif what_to_do == 2:
-            data[where_to_put] = int(data[input_1]) * int(data[input_2])
-        else:
-            print(what_to_do)
-            return(data)
-
-
-answer_2 = pairs_needed(data)
+answer_2 = intcode(data)
 print("Part 2: " + str(answer_2))
