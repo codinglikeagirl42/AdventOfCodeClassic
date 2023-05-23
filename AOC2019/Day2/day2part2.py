df67c202897f6378a3919cabf9221af9773e1e5b
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-05-23 15:11:18 -0500

    Got part2 working

--- AOC2019/Day2/day2part2.py
+++ AOC2019/Day2/day2part2.py
@@ -1,20 +1,18 @@
 #Get data from text file
 data = open("AOC2019/Day2/day2.txt", "r").read().split(",")
 
-def intcode(data, noun, verb):
-    for opcode in range(0, (len(data)-4), 4):
-        test_data = data
-        test_data[1] = noun
-        test_data[2] = verb
-        print(test_data[1])
-        print(test_data[2])
+# https://www.educative.io/answers/how-to-copy-a-list-in-python
+
+def intcode(test_data):
+    for opcode in range(0, (len(test_data)-4), 4):
+        
         opcode = int(opcode)
         what_to_do = int(test_data[opcode])
         input_1 = int(test_data[opcode + 1])
         input_2 = int(test_data[opcode + 2])
         
         if what_to_do == 99:
-            return test_data[0]
+            return test_data
         
         #where_to_put = int(test_data[opcode + 3])
 
@@ -24,17 +22,27 @@ def intcode(data, noun, verb):
             test_data[int(test_data[opcode + 3])] = int(test_data[input_1]) * int(test_data[input_2])
         #else:
             #return test_data[0]
-"""    
+  
 #Part 1
+test_data = data[:]
+test_data[1] = 12
+test_data[2] = 2
+       
+answer = intcode(test_data)
+print("Part 1: " + str(answer[0]))
 
-answer = intcode(data, 12, 2)
-print("Part 1: " + str(answer))
-"""
+
 for noun in range(100):
     for verb in range(100):
-        check = intcode(data, noun, verb)
-        print(check)
-        if check == 19690720:
+
+        test_data = data[:]
+        test_data[1] = noun
+        test_data[2] = verb
+
+        #print(test_data[0:3])
+        check = intcode(test_data)
+        #print(check[0])
+        if check[0] == 19690720:
             print("Part 2: " + str(100 * noun + verb))
-            
- 
+           
+                      
